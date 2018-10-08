aa7c81276e6bac698b6b3014341ca49b47a2d82d
Pukhov Vsevolod <pukhov.vyu@phystech.edu>
2018-10-08 21:49:51 +0300

    Task_1: error-handling defines, small fixes

--- Task_1/fifo.c
+++ Task_1/fifo.c
@@ -7,14 +7,13 @@
 #include <errno.h>
 #include <inttypes.h>
 
-/// Fifo with pid list
-const char   FIFO_QUEUE_PATH[] = "queue.fifo";
-
+// Value to identify pair
 struct ident_t {
 	pid_t pid;
 };
 
-/// Channels for sender + receiver pairs
+/// Fifo with pid list
+const char   FIFO_QUEUE_PATH[] = "queue.fifo";
 const char   FIFO_CHANNEL_PATH_PREFIX[] = "channel.";
 const int    FIFO_CHANNEL_PATH_MAX = sizeof(FIFO_CHANNEL_PATH_PREFIX) + \
 				     sizeof(struct ident_t) * 2;
@@ -25,8 +24,9 @@ const mode_t FIFO_MODE = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP	|	\
 const int FIFO_MAX_SLEEP_US = ((int) 2) << 20;
 const int FIFO_MIN_SLEEP_US = ((int) 2) << 6;   
 
+/// Copy buffer size (atomic)
+const int FDTOFD_CPY_BUF_SIZE = 512;
 
-#define FDTOFD_CPY_BUF_SIZE 512
 ssize_t buftofd_cpy(int fd_out, char *buf, size_t count)
 {	
 	ssize_t tmp = 1;
@@ -150,9 +150,18 @@ int receiver_wait_byte(int fd)
 		usleep(delay);
 	}
 	return 0;
-}
+}	
 
 
+#define RECEIVER_ERR_RET(str_msg)			\
+do {							\
+	perror("Error: receiver: " str_msg);		\
+	fprintf(stderr, "\tLine %d\n", __LINE__);	\
+	unlink(channel_path);				\
+	free(channel_path);				\
+	return -1;					\
+} while (0)		
+	
 int receiver()
 {	
 	// Prepare channel
@@ -161,63 +170,52 @@ int receiver()
 	get_channel_path(&channel_path, id);
 	init_channel(channel_path, FIFO_MODE);
 	int fd_channel = open(channel_path, O_RDONLY | O_NONBLOCK);
-	if (fd_channel == -1) {
-		perror("Error: open");
-		return -1;
-	}
+	if (fd_channel == -1)
+		RECEIVER_ERR_RET("open channel");
 	
 	// Open queue fifo and write id
-	if (mkfifo(FIFO_QUEUE_PATH, FIFO_MODE) == -1 && errno != EEXIST) {
-		perror("Error: mkfifo");
-		free(channel_path);
-		return -1;
-	}
+	if (mkfifo(FIFO_QUEUE_PATH, FIFO_MODE) == -1 && errno != EEXIST)
+		RECEIVER_ERR_RET("mkfifo queue");
+		
 	int fd_queue = open(FIFO_QUEUE_PATH, O_RDWR);
-	if (fd_queue == -1) {
-		perror("Error: open");
-		free(channel_path);
-		return -1;
-	}
-	if (write(fd_queue, &id, sizeof(id)) != sizeof(id)) {
-		perror("Error: write");
-		free(channel_path);
-		return -1;
-	}
+	if (fd_queue == -1)
+		RECEIVER_ERR_RET("open queue");
+	
+	if (write(fd_queue, &id, sizeof(id)) != sizeof(id))
+		RECEIVER_ERR_RET("write id to queue");
 	
 	// Wait for sender
-	if (receiver_wait_byte(fd_channel) != 1) {
-		fprintf(stderr, "Error: It seems the sender is dead\n");
-		free(channel_path);
-		return -1;
-	}
+	if (receiver_wait_byte(fd_channel) != 1)
+		RECEIVER_ERR_RET("It seems the sender is dead\n");
 	
 	// Close queue fifo
-	if (close(fd_queue) == -1) {
-		perror("Error: close");
-		free(channel_path);
-		return -1;
-	}
+	if (close(fd_queue) == -1)
+		RECEIVER_ERR_RET("close queue");
 	
-	if (fcntl(fd_channel, F_SETFL, O_RDONLY) == -1) {
-		perror("Error: fcntl");
-		free(channel_path);
-		return -1;
-	}
-	if (fdtofd_cpy(STDOUT_FILENO, fd_channel) == -1) {
-		perror("Error: fdtofd_cpy");
-		free(channel_path);
-		return -1;
-	}
-	if (close(fd_channel) == -1) {
-		perror("Error: close");
-		free(channel_path);
-		return -1;
-	}
+	if (fcntl(fd_channel, F_SETFL, O_RDONLY) == -1)
+		RECEIVER_ERR_RET("fcntl channel");
+		
+	if (fdtofd_cpy(STDOUT_FILENO, fd_channel) == -1)
+		RECEIVER_ERR_RET("copy from channel to stdout");
+		
+	if (close(fd_channel) == -1)
+		RECEIVER_ERR_RET("close channel");
 	
 	unlink(channel_path);
 	free(channel_path);
 	return 0;
 }
+
+#undef RECEIVER_ERR_RET
+
+#define SENDER_ERR_RET(str_msg)				\
+do {							\
+	perror("Error: sender: " str_msg);		\
+	fprintf(stderr, "ErrLine %d\n", __LINE__);	\
+	unlink(channel_path);				\
+	free(channel_path);				\
+	return -1;					\
+} while (0)	
 
 int sender(const char *inp_path)
 {
@@ -251,34 +249,27 @@ int sender(const char *inp_path)
 	char *channel_path;
 	get_channel_path(&channel_path, id);
 	int fd_channel = open(channel_path, O_NONBLOCK | O_WRONLY);
-	if (fd_channel == -1) {
-		perror("Error: open");
-		return -1;
-	}
-	free(channel_path);
-	if (fcntl(fd_channel, F_SETFL, O_WRONLY) == -1) {
-		perror("Error: fcntl");
-		return -1;
-	}
+	if (fd_channel == -1) 
+		SENDER_ERR_RET("It seems the receiver is dead");
+		
+	if (fcntl(fd_channel, F_SETFL, O_WRONLY) == -1)
+		SENDER_ERR_RET("fcntl channel");
 	
 	// Sending msg that sender is ready
 	char msg = 1;
-	if (buftofd_cpy(fd_channel, &msg, sizeof(msg)) == -1) {
-		perror("Error: buftofd_cpy");
-		return -1;
-	}
+	if (buftofd_cpy(fd_channel, &msg, sizeof(msg)) == -1)
+		SENDER_ERR_RET("send 1byte-msg");
 	
-	// write data to channel (blocking mode)
-	// write to closed for read -> SIGPIPE
+	// Open input file
 	int fd_inp = open(inp_path, O_RDONLY);
-	if (fd_inp == -1) {
-		perror("Error: open");
-		return -1;
-	}
-	if (fdtofd_cpy(fd_channel, fd_inp) == -1) {
-		perror("Error: fdtofd_cpy");
-	}
-	
+	if (fd_inp == -1) 
+		SENDER_ERR_RET("open input file");
+		
+	if (fdtofd_cpy(fd_channel, fd_inp) == -1)
+		SENDER_ERR_RET("copy from input to channel");
+		
+	unlink(channel_path);
+	free(channel_path);
 	return 0;
 }
 
@@ -287,10 +278,10 @@ int main(int argc, char *argv[])
 {
 	if (argc == 2) {
 		if (sender(argv[1]) == -1)
-			perror("Error: sender");
+			fprintf(stderr, "Error: sender failed\n");
 	} else if (argc == 1) {
 		if (receiver() == -1)
-			perror("Error: receiver");
+			fprintf(stderr, "Error: receiver failed\n");
 	} else 
 		fprintf(stderr, "Error: wrong argument list\n");
 	return 0;

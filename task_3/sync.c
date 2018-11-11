3a48e4f889c5725a1efda17033813983b603264a
Pukhov Vsevolod <pukhov.vyu@phystech.edu>
2018-11-11 21:49:48 +0300

    Task_3: new sync

---
+++ task_3/sync.c
@@ -0,0 +1,75 @@
+#include "sync.h"
+
+
+int pair_capture(int semid, struct syncbuf self, struct syncbuf othr, int (*self_init)(int))
+{
+	struct sembuf sop_buf[4];
+	int n_sop = 0;
+	
+	// Capture lock
+	SOPBUF_ADD(self.lock,  0, 0);
+	SOPBUF_ADD(self.actv,  0, 0);
+	SOPBUF_ADD(othr.actv,  0, 0);
+	SOPBUF_ADD(self.lock,  1, SEM_UNDO);
+	SOPBUF_SEMOP();
+	
+	// User-defined init
+	if (self_init != NULL && (*self_init)(semid) == -1) {
+		fprintf(stderr, "Error: self_init failed\n");
+		return -1;
+	}
+	
+	// Activate self
+	SOPBUF_ADD(othr.lock, -1, 0);
+	SOPBUF_ADD(othr.lock,  1, 0);
+	SOPBUF_ADD(self.actv,  1, SEM_UNDO);
+	SEMBUF_SEMOP();
+	
+	// Wait until the other is activated
+	SOPBUF_ADD(othr.lock, -1, IPC_NOWAIT);
+	SOPBUF_ADD(othr.lock,  1, 0);
+	SOPBUF_ADD(othr.actv, -1, 0);
+	SOPBUF_ADD(othr.actv,  1, 0);
+	SEMBUF_SEMOP();
+	
+	return 0;
+}
+
+
+int pair_release(int semid, struct syncbuf self, struct syncbuf othr)
+{
+	struct sembuf sop_buf[5];
+	int n_sop = 0;
+	
+	// Process ready for exit
+	SOPBUF_ADD(self.done,  1, SEM_UNDO);
+	if (SOPBUF_SEMOP() == -1) {
+		perror("Error: semop");
+		return -1;
+	}
+	
+	// Wait for othr, release lock
+	SOPBUF_ADD(othr.actv, -1, IPC_NOWAIT);
+	SOPBUF_ADD(othr.actv,  1, 0);
+	SOPBUF_ADD(othr.done, -1, 0);
+	SOPBUF_ADD(othr.done,  1, 0);
+	SOPBUF_ADD(self.lock, -1, SEM_UNDO);
+	if (SOPBUF_SEMOP() == -1) {
+		if (errno == EAGAIN)
+			fprintf(stderr, "Error: othr process failed\n");
+		else
+			perror("Error: semop");
+		return -1;
+	}
+	
+	// Success
+	
+	SOPBUF_ADD(othr.lock,  0, 0);
+	SOPBUF_ADD(self.actv, -1, SEM_UNDO);
+	if (SOPBUF_SEMOP() == -1) {
+		perror("Error: semop");
+		return -1;
+	}
+	
+	return 0;
+}
---
+++ task_3/sync.h
@@ -0,0 +1,43 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+// #include <fcntl.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <errno.h>
+
+union semun {
+	int          	 val;	/* Value for SETVAL */
+	struct semid_ds *buf;	/* Buffer for IPC_STAT, IPC_SET */
+	unsigned short  *array;	/* Array for GETALL, SETALL */
+	struct seminfo  *__buf;	/* Buffer for IPC_INFO
+				(Linux-specific) */
+};
+
+struct syncbuf {
+	unsigned short lock;
+	unsigned short actv;
+	unsigned short done;
+};
+
+#define SOPBUF_ADD(num, op, flg)		\
+	do {					\
+		sop_buf[sop_n].sem_num = num;	\
+		sop_buf[sop_n].sem_op  = op;	\
+		sop_buf[sop_n].sem_flg = flg;	\
+		sop_n++;			\
+	} while (0)
+
+#define SOPBUF_SEMOP()						\
+	({							\
+		int sop_status = semop(semid, sop_buf, sop_n);	\
+		sop_n = 0;					\
+		sop_status;					\
+	})
+
+
+int pair_capture(int semid, struct syncbuf self, struct syncbuf othr, int (*self_init)(int));
+int pair_release(int semid, struct syncbuf self, struct syncbuf othr);
